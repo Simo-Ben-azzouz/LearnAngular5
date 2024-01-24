2cc45cc672d3b0652bfe4ad3fdb4d014f0973a19
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-01-24 15:13:37 +0100

    ngSwitchCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  viewCourse = 'Laravel';
   title = 'Angular5v2';
   body ='Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum. Wikipédia'
 }
