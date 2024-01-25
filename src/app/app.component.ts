ec0e4339e8064c752b0ad8c252b283b7e6eaaae2
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-01-25 09:40:07 +0100

    ngFor-trackBy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,20 @@ export class AppComponent {
   title = 'Angular5v2';
   body ='Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum. Wikipédia'
 
-  courses = [
-    {id :1 , title : "Laravel"},
-    {id :2 , title : "Angular"},
-    {id :3 , title : "VueJS"},
-    {id :4 , title : "ReactJS"}    
-  ];
-
+  courses:any;
+trackCourse(index:any , course:any)
+{
+  return course ? course.id : undefined;
+}
+  loadCourse()
+  {
+    this.courses = [
+      {id :1 , title : "Laravel"},
+      {id :2 , title : "Angular"},
+      {id :3 , title : "VueJS"},
+      {id :4 , title : "ReactJS"}    
+    ];
+  }
   addCourse()
   {
     this.courses.push({id :6 , title : "Firebase"});
