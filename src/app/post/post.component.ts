7afc60266945e1cd4b6e9dc10bd9d807e9c8928e
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-01-31 20:16:15 +0100

    httpClient-add-data

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpContext } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { response } from 'express';
 
 @Component({
   selector: 'app-post',
@@ -9,12 +10,35 @@ import { Component } from '@angular/core';
 export class PostComponent {
 
   posts : any[] = [];
+  title: any;
+  body: any;
+  post : any = {
+    title : "" ,
+    body : "",
+    id: 0,
+  }
+
   constructor (private http : HttpClient)
   {
     this.http.get('https://jsonplaceholder.typicode.com/posts')
               .subscribe((response : any)  =>{
-              this.posts = response;
-                
+              this.posts = response;               
               });
+  }
+
+  createPost()
+  {
+    this.http.post('https://jsonplaceholder.typicode.com/posts', this.post)
+    .subscribe( (response : any) => 
+    {
+      this.post.id = response.id;
+      this.posts.unshift(this.post);
+
+      this.post = {
+        title : "" ,
+        body : "",
+        id: 0,
+      }
+    });
   }
 }
