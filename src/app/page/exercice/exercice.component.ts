a560b8a8dfe7bf51f803596ed8dcf61d623508c2
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-11 18:35:09 +0100

    setup-all-add-follower-service

---
+++ src/app/page/exercice/exercice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercice.component.html',
+  styleUrl: './exercice.component.css'
+})
+export class ExerciceComponent {
+
+}
