4e51c6ca6bd1261f317cd9b2321756d355093d26
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-11 21:06:04 +0100

    add-inject

--- src/app/page/exercice/exercice.component.ts
+++ src/app/page/exercice/exercice.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FollowerService } from '../../service/follower.service';
+import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-exercice',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exercice.component.html',
   styleUrl: './exercice.component.css'
 })
 export class ExerciceComponent {
+  /**
+   *
+   */
+  followerService = inject(FollowerService);
+
+  posts : any[] = [];
+  title: any;
+  body: any;
+  post : any = {
+    title : "" ,
+    body : "",
+    id: 0,
+  }
+  ngOnInit()
+  {
+    this.followerService.getAll()
+    .subscribe((response : any)  =>{
+      console.log(this.posts); // Log the posts array to the console
+      this.posts = response;
+    });
+  }
+
+  // getAll() {
+  //   return this.followerService.getAll()
+  //   .subscribe((response : any)  =>{
+  //     console.log(this.posts); // Log the posts array to the console
+  //     this.posts = response;
+  //   });
+  // }
 
 }
--- src/app/service/follower.service.ts
+++ src/app/service/follower.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FollowerService {
 
-  constructor() { }
+  private urlAPI = 'https://jsonplaceholder.typicode.com/posts';
+  // constructor(private http : HttpClient) { }
+  http = inject(HttpClient);
+  getAll() {
+    return this.http.get(this.urlAPI)
+      
+  }
 }
