e1a665239b548d4c91bda9b33c8cfc08bd638544
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-13 11:11:12 +0100

    add-all-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { ExerciceComponent } from './page/exercice/exercice.component';
+import { HomeComponent } from './page/home/home.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path : '',
+        component : HomeComponent
+    },
+    {
+        path : 'following',
         component : ExerciceComponent
-    }
+    },
+
+
+    {
+        path : '**',
+        component : NotFoundComponent
+    },
+
+
 ];
