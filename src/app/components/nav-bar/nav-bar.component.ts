1586631e4a481c8f799665e40594f5ea60bd444a
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-14 17:14:41 +0100

    remove-bootstrap-and-use-tailwind

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink,RouterModule,CommonModule],
+  imports: [RouterLink,RouterModule,RouterOutlet,CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
